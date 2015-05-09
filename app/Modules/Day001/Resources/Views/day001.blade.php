4896fb1409944b271160eed1d219e60c7922bd1f
Joel (home) <joel@kumuwai.com>
2015-05-08 21:42:35 -1000

    include formatting and logos

--- app/Modules/Day001/Resources/Views/day001.blade.php
+++ app/Modules/Day001/Resources/Views/day001.blade.php
@@ -6,23 +6,24 @@
     <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-<div class="container">
+<div class="wrapper">
     <h1>Aloha!</h1>
 
     <p>I'm Joel, and this is a playground for me to spend a little time to experiment with web tools and techniques (both client and server side). I'd generally like to keep these as simple projects--maybe spend an hour or two on each. As of right now, I plan to be playing with:</p>
 
-    <ul>
-        <li><a href="http://laravel.com">Laravel 5</a></li>
-        <li><a href="https://phpunit.de/">phpunit</a></li>
-        <li><a href="http://codeception.com/">codeception</a></li>
-        <li><a href="http://getbootstrap.com/">bootstrap</a></li>
-        <li><a href="http://jquery.com/">jquery</a></li>
-        <li><a href="https://angularjs.org/">angularjs</a></li>
-        <li><a href="http://bower.io">bower</a></li>
-        <li><a href="http://gulpjs.com/">gulp</a></li>
-        <li><a href="http://lesscss.org/">less</a></li>
-        <li><a href="https://stripe.com">Stripe</a></li>
-    </ul>
+    <div class="inline-image">
+        @include('day001::image', ['url'=>'http://laravel.com', 'file'=>'logo_laravel.png','caption'=>'Laravel 5'])
+        @include('day001::image', ['url'=>'https://phpunit.de', 'file'=>'logo_phpunit.gif','caption'=>'phpunit'])
+        @include('day001::image', ['url'=>'https://codeception.com', 'file'=>'logo_codeception.png','caption'=>'codeception'])
+        @include('day001::image', ['url'=>'http://getbootstrap.com', 'file'=>'logo_bootstrap.svg', 'caption'=>'bootstrap'])
+        @include('day001::image', ['url'=>'http://jquery.com', 'file'=>'logo_jquery.svg', 'caption'=>'jquery'])
+        @include('day001::image', ['url'=>'http://jqueryui.com', 'file'=>'logo_jqueryui.svg', 'caption'=>'jquery ui'])
+        @include('day001::image', ['url'=>'https://angularjs.org', 'file'=>'logo_angular.svg', 'caption'=>'angularjs'])
+        @include('day001::image', ['url'=>'http://bower.io', 'file'=>'logo_bower.png', 'caption'=>'bower'])
+        @include('day001::image', ['url'=>'http://gulpjs.com', 'file'=>'logo_gulp.png', 'caption'=>'gulp'])
+        @include('day001::image', ['url'=>'http://lesscss.org', 'file'=>'logo_less.png', 'caption'=>'less'])
+        @include('day001::image', ['url'=>'https://stripe.com', 'file'=>'logo_stripe.png', 'caption'=>'stripe'])
+    </div>
 
     <p>... and anything else that looks interesting and fun. :-) The full project source is available on github.</p>
 
---
+++ app/Modules/Day001/Resources/Views/image.blade.php
@@ -0,0 +1,6 @@
+<figure>
+    <a href="{{$url}}">
+        <img src="/img/logos/{{$file}}" style="max-height:60px">
+        <figcaption>{{$caption}}</figcaption>
+    </a>
+</figure>
