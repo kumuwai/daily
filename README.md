97c1c6b4d33c2eaea2b841510a66e94b08992166
Joel (home) <joel@kumuwai.com>
2015-05-29 09:44:37 -1000

    include versioneye

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Playground
 [![Build Status](https://img.shields.io/travis/kumuwai/playground/master.svg)](https://travis-ci.org/kumuwai/playground)
 [![Coverage Status](https://coveralls.io/repos/kumuwai/playground/badge.png?branch=master)](https://coveralls.io/r/kumuwai/playground)
 [![Quality Score](https://img.shields.io/scrutinizer/g/kumuwai/playground.svg)](https://scrutinizer-ci.com/g/kumuwai/playground)
+[![Dependency Status](https://www.versioneye.com/user/projects/5568bc156365320015351400/badge.svg?style=flat)](https://www.versioneye.com/user/projects/5568bc156365320015351400)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE.md)
 
 This is a playground for me to spend a little time to experiment with web development tools and techniques (both client and server side). I'd generally like to keep these as simple projects--maybe spend a few hours on each. I plan to be playing with:
--- app/Modules/Project001/Resources/Views/project001.blade.php
+++ app/Modules/Project001/Resources/Views/project001.blade.php
@@ -39,6 +39,7 @@
 <p><a href="https://travis-ci.org/kumuwai/playground"><img alt="Build Status" src="https://img.shields.io/travis/kumuwai/playground/master.svg" style="max-width:100%;"></a>
 <a href="https://coveralls.io/r/kumuwai/playground"><img alt="Coverage Status" src="https://coveralls.io/repos/kumuwai/playground/badge.png?branch=master" style="max-width:100%;"></a>
 <a href="https://scrutinizer-ci.com/g/kumuwai/playground"><img alt="Code Quality" src="https://img.shields.io/scrutinizer/g/kumuwai/playground.svg" style="max-width:100%;"></a>
+<a href='https://www.versioneye.com/user/projects/5568bc156365320015351400'><img src='https://www.versioneye.com/user/projects/5568bc156365320015351400/badge.svg?style=flat' alt="Dependency Status" /></a>
 <a href="https://github.com/kumuwai/playground/blob/master/LICENSE.md"><img alt="License" src="https://img.shields.io/badge/license-MIT-blue.svg" style="max-width:100%;"></a></p>
 
     <div id="main-menu">
