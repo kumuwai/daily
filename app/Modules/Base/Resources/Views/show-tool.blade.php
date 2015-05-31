ac09c92c60232f8ec932db5aae99f1df00ace491
Joel (home) <joel@kumuwai.com>
2015-05-30 18:00:21 -1000

    tweaks to front-end

--- app/Modules/Base/Resources/Views/show-tool.blade.php
+++ app/Modules/Base/Resources/Views/show-tool.blade.php
@@ -10,37 +10,32 @@
 @stop
 
 @section('page-title')
-    <h1>
-        @if( $tool )
+@stop
+
+@section('content')
+
+    @if( ! $projects->count() )
+        <h1>{{$id}}</h1>
+        {{ "No projects use this tool" }}
+    @else
+        <h1>
             <a href="{{$tool[$id]['url']}}">
                 {{ $tool[$id]['caption'] }}
 
                 <figure style="display:inline-block">
                     <img style="max-height: 30px" src="/img/logos/{{$tool[$id]['file']}}">
                 </figure>
-
             </a>
-        @else
-            {{ $id }}
-        @endif        
-    </h1>
-@stop
-
-@section('content')
-
-    <div id="project-menu">
-
-    @if( ! $projects->count() )
-        {{ "No projects use this tool" }}
-    @else
+        </h1>
+        <h3>These projects demonstrate {{$tool[$id]['caption']}}:</h3>
+        <div id="project-menu">
         @foreach($projects as $project)
             <div class="menu-item">
                 <div class="name"><a href="/{{$project->slug}}">{{$project->title}}</a></div> 
                 <div class="description">{{$project->description}} {!! $project->tools !!}</div>
             </div>
         @endforeach
+        </div>
     @endif
-
-    </div>
 
 @stop
