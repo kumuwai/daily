58dda56256e47fbdf4cee55325e56380f278444a
Joel Hatch <jijoel@yahoo.com>
2016-07-27 05:21:07 +0000

    split gulpfiles into locally generated and vendor files

---
+++ gulpfile-vendor.js
@@ -0,0 +1,26 @@
+// Update vendor files, using:
+//
+//  bower update
+//  gulp --gulpfile gulpfile-vendor.js  
+//
+var elixir = require('laravel-elixir');
+
+var bower_source = 'resources/assets/bower/';
+
+elixir(function(mix) {
+    mix
+    .scripts([
+        '/../bower/angular/angular.js',
+        '/../bower/angular-resource/angular-resource.js',
+        '/../bower/angular-route/angular-route.js',
+    ], 'public/js/angular.js')
+    .copy(bower_source + 'jquery/dist/*.*', 'public/js')
+    .copy(bower_source + '/bootstrap/dist/js/*.*', 'public/js')
+    .copy(bower_source + '/vue/dist/*.js', 'public/js')
+    .copy(bower_source + '/highlight', 'public/vendor/highlight')
+    .copy(bower_source + '/jplayer/dist', 'public/vendor/jplayer')
+    ;
+    // .scripts(['forum.js', 'threads.js'], 'public/js/forum.js');
+});
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,3 @@
-var elixir = require('laravel-elixir');
-
-var bower_source = 'resources/assets/bower/';
-
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,25 +8,16 @@ var bower_source = 'resources/assets/bower/';
  | file for our application, as well as publishing vendor resources.
  |
  */
+ 
+var elixir = require('laravel-elixir');
+
+var bower_source = 'resources/assets/bower/';
+
+require('laravel-elixir-vueify');
 
 elixir(function(mix) {
     mix.less('app.less');
-});
-
-elixir(function(mix) {
-    mix
-    .scripts([
-        '/../bower/angular/angular.js',
-        '/../bower/angular-resource/angular-resource.js',
-        '/../bower/angular-route/angular-route.js',
-    ], 'public/js/angular.js')
-    .copy(bower_source + 'jquery/dist/*.*', 'public/js')
-    .copy(bower_source + '/bootstrap/dist/js/*.*', 'public/js')
-    .copy(bower_source + '/vue/dist/*.js', 'public/js')
-    .copy(bower_source + '/highlight', 'public/vendor/highlight')
-    .copy(bower_source + '/jplayer/dist', 'public/vendor/jplayer')
-    ;
-    // .scripts(['forum.js', 'threads.js'], 'public/js/forum.js');
+    mix.browserify('main.js');
 });
 
 
