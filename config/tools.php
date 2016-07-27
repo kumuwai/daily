cac7cb1f06b326fe8bad28ed0c6c918e5f5bc5c4
Joel Hatch <jijoel@yahoo.com>
2016-07-27 05:20:04 +0000

    include browserify

--- config/tools.php
+++ config/tools.php
@@ -11,6 +11,7 @@ return [
     'bower' => ['caption'=>'bower', 'url'=>'http://bower.io', 'file'=>'logo_bower.png'],
     'vue' => ['caption'=>'Vue.js', 'url'=>'https://vuejs.org', 'file'=>'logo_vue.png'],
     'jplayer' => ['caption'=>'JPlayer', 'url'=>'http://jplayer.org', 'file'=>'logo_jplayer.png'],
+    'browserify' => ['caption'=>'Browserify', 'url'=>'http://browserify.org/', 'file'=>'logo_browserify.png'],
     
     // 'jquery' => ['caption'=>'jquery', 'url'=>'http://jquery.com', 'file'=>'logo_jquery.svg'],
     // 'jquery-ui' => ['caption'=>'jquery ui', 'url'=>'http://jqueryui.com', 'file'=>'logo_jqueryui.svg'],
